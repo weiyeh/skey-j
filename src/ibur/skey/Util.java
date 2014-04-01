1f5138ac260b8a434cedcc0ff6805432851cdc1b
Iburinoc <iburinoc@gmail.com>
2014-04-01 00:00:13 -0400

    Adding dropbox support

--- src/ibur/skey/Util.java
+++ src/ibur/skey/Util.java
@@ -4,4 +4,10 @@ import java.security.SecureRandom;
 
 public class Util {
 	public static final SecureRandom r = new SecureRandom();
+	
+	public static final String OS = System.getProperty("os.name");
+	
+	public static boolean isWindows() {
+		return OS.startsWith("Windows");
+	}
 }
---
+++ src/ibur/skey/desktop/Dropbox.java
@@ -0,0 +1,54 @@
+package ibur.skey.desktop;
+
+import ibur.lib.B64;
+import ibur.skey.Util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Dropbox {
+	public static File getDropboxFolder() throws Exception {
+		try{
+			String dbPath;
+			if(Util.isWindows()) {
+				dbPath = System.getenv("APPDATA") + File.separator
+						+ "Dropbox" + File.separator + 
+						"host.db";
+				
+			} else {
+				dbPath = System.getProperty("user.home") + File.separator
+						+ ".dropbox" + File.separator + 
+						"host.db"; 
+			}
+			File db = new File(dbPath);
+			System.out.println(dbPath);
+			System.out.println(Arrays.toString(dbPath.toCharArray()));
+			if(!db.exists()) {
+				throw new RuntimeException("No Dropbox host.db, Dropbox is required for this program to sync");
+			}
+			BufferedReader br = new BufferedReader(new FileReader(db));
+			br.readLine();
+			String encodedPath = br.readLine();
+			byte[] decodedPath = B64.decode(encodedPath);
+			String dropboxPath = new String(decodedPath, "UTF-8");
+			System.out.println(dropboxPath);
+			File dropboxFolder = new File(dropboxPath);
+			if(!dropboxFolder.exists()) {
+				throw new RuntimeException("No Dropbox folder, Dropbox is required for this program to sync");
+			}
+			return dropboxFolder;
+		}
+		catch(IOException e) {
+			throw e;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		File db = getDropboxFolder();
+		File pws = new File(db.getAbsolutePath() + File.separator + ".skey.dat");
+		pws.createNewFile();
+	}
+}
